9a77199c8e4fa8b0c8afd2f80856285e591abd42
samkc-0 <sam.k.c.0@icloud.com>
2025-10-31 16:20:48 +0000

    update links

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 # this script build the repo for github pages
 # make sure you have a .env with REPONAME set
-source .env
-if [ -z "$REPONAME" ]; then
-  echo "REPONAME is not set in .env"
-  exit 1
-fi
+# source .env
+# if [ -z "$REPONAME" ]; then
+#   echo "REPONAME is not set in .env"
+#  exit 1
+#fi
+
 mkdir -p docs
-python src/main.py "$REPONAME/" docs
+python src/main.py "/" docs
